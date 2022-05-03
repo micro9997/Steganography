010867d1332903d351634651a1c47cb34c46e87e
Mohanakrishna Kumare <micro9997@gmail.com>
2022-05-03 14:35:24 +0530

    Cleaned

---
+++ main.c
@@ -0,0 +1,20 @@
+
+// #include <stdio.h>
+// #include <string.h>
+
+// int main(int argc, char **argv) {
+//     printf("%d\n", argc);
+//     printf("%s\n", *(argv + 1));
+//     if(*(argv + 1) == "-e") {
+//         printf("Yes\n");
+//     } else {
+//         printf("No\n");
+//     }
+
+//     return 0;
+// }
+
+// // char **argv
+// // char *argv
+// // char argv
+// // char &argv
--- poc.c
+++ poc.c
@@ -1,8 +1,62 @@
 
-#include <stdio.h>
-#include <string.h>
+// #include <stdio.h>
+// #include <string.h>
 
-int main() {
 
-    return 0;
-}
+// int main () {
+//    const char haystack[20] = "Tutorialsoint";
+//    const char needle[10] = "Point";
+//    char *ret;
+
+//    ret = strstr(haystack, needle);
+
+//    printf("The substring is: %s\n", ret);
+   
+//    return(0);
+// }
+
+// #include <stdio.h>
+// #include <string.h>
+
+// int main() {
+//     char ch[] = {'A', 'B', 'C', 'D', 'E'};
+//     char (*ptr)[5] = &ch;
+
+//     printf("%c\n", *(ch + 1));
+//     printf("%c\n", *(ptr + 1));
+//     // printf("%s\n", ptr);
+
+//     // printf("Ivalid Input \n please pass Encode : ./a.out -e beautiful.bmp secret.txt stego.bmp\n Decode : .a/out -d stego.bmp decode.txt\n");
+
+//     return 0;
+// }
+
+// printf("choosen encodeing process\n");
+//     printf("SUCESS : Read and validate of encode args\n");
+//     printf("Encodeing process started ......\n");
+//         printf("SUCESS: Enodeing process\n");
+//         printf("Falies: Encoding process\n");
+//     printf("Failes : Read and validate of encode args\n");
+// printf("choosen decodeing process\n");
+// printf("Ivalid Input \n please pass Encode : ./a.out -e beautiful.bmp secret.txt stego.bmp\n Decode : .a/out -d stego.bmp decode.txt\n");
+
+
+// printf("SUCESS: OPEN FILES\n");
+//     printf ("SUCCESS: Check capacity\n");
+//         printf("SUCCESS: Copy bmp header\n");
+//             printf("SUCCESS: Magic string encodeing\n");
+//                 printf( "SUCESS : SECRET FILE EXTENSION SIZE\n");
+//                     printf("SUCESS: SECRETE FILE EXTENSION\n");
+//                         printf( "SUCCESS: ENCODE SECRETE FILE SIZE \n");
+//                             printf("SUCCESS: ENCODING SECRETE DATA\n" );
+//                                 printf("SUCCESS: CPOY BMP REMAING DATA\n");
+
+//                                 printf("FAILES: CPOY BMP REMAING DATA\n");
+//                             printf("FAILES: ENCODING SECRETE DATA\n");
+//                         printf("FAILES: ENCODE SECRETE FILE SIZE \n");
+//                     printf("FAILES : SECRETE FILE EXTENSION\n");
+//                 printf("FALIES: SECRET_FILE EXTENSION SIZE\n");
+//             printf("FAILES: Magic string encodeing\n");
+//         printf ("FAILES: Copy bmp header \n");
+//     printf ("FAILES: Check capacity\n");
+// printf("FAILES: OPEN FILES\n");
